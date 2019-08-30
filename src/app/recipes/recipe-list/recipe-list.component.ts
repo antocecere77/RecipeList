cb5334244ead31a2ce6729c192fd2713ed4a1400
Antonio Cecere <antocecere77@gmail.com>
2019-08-30 07:03:23 +0200

    Aggiunta recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('A Test Recipe', 'This is a simply test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'),    
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'),
     // tslint:disable-next-line:max-line-length
     new Recipe('A Test Recipe', 'This is a simply test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
   ];

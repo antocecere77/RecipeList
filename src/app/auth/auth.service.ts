657dce3409bd15fd616f3629cc346c5a3b5817bd
Antonio Cecere <antocecere77@gmail.com>
2019-10-17 07:34:13 +0200

    Aggiunta gestione login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -37,5 +38,15 @@ constructor(private http: HttpClient) {}
                 }
                 return throwError(errorMessage);
             }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBk9qH4vga9mFY0CbsviIK0yWWU8SpnHHM',
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
     }
 }

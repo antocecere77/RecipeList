1008f06f202eeb281bccc407d7d9b35beeffe9f8
Antonio Cecere <antocecere77@gmail.com>
2019-10-21 06:59:14 +0200

    Modificata la ui dopo il login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
@@ -10,7 +11,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthComponent {
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     isLoginMode = true;
     isLoading = false;
@@ -40,6 +41,7 @@ export class AuthComponent {
             resData => {
                 console.log(resData);
                 this.isLoading = false;
+                this.router.navigate(['/recipes']);
             },
             errorMessage => {
                 console.log(errorMessage);

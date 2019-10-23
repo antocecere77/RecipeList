493fa9324f5c301bfefa7dac8e5364f1c3bba449
Antonio Cecere <antocecere77@gmail.com>
2019-10-23 06:53:43 +0200

    Aggiunto token ad interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -19,20 +19,14 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-       return this.authService.user.pipe(
-           take(1),
-           exhaustMap(user => {
-             return this.http.get<Recipe[]>(
-                 'https://recipe-book-ecdb4.firebaseio.com/recipe.json',
-                 {
-                     params: new HttpParams().set('auth', user.token)
-                 }
-             );
-           }),
+        return this.http.get<Recipe[]>(
+                'https://recipe-book-ecdb4.firebaseio.com/recipe.json'
+             ).pipe(
            map(recipes => {
             return recipes.map(recipe => {
-                return {...recipe,
-                        ingredients: recipe.ingredients ? recipe.ingredients : []
+                return {
+                    ...recipe,
+                    ingredients: recipe.ingredients ? recipe.ingredients : []
                 };
             });
            }),
